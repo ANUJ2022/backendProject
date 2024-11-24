f295b76a3044faaf83a93427d7b539b0c0d824d8
Anuj Negi <anujnegi234@gmail.com>
2024-11-24 17:48:49 +0530

    github data added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 
 const port = 4000;
 const app = express();
+const name = "NHI btaunga"
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -12,7 +13,9 @@ app.get("/twitter", (req, res) => {
     console.log("Twitter route hit");
     res.send("Hello Twitter");
 });
-
+app.get("/json", (req, res) => {
+    res.json(name);
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`App is running on port ${port}`);
